a4fce634f13147a796ebd3ec3a5e92fee59dc9fb
BliznyukAlx <bliznyuk.alx@gmail.com>
2020-02-10 10:31:15 +0200

    init project

---
+++ src/main/java/com/ma/hookahMarket/HookahMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ma.hookahMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookahMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookahMarketApplication.class, args);
+	}
+
+}
